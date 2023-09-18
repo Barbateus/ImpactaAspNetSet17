697370e6259bd18336ffff2806991e809201bcc9
User <User@DESKTOP-2KR9P03>
2023-09-17 22:32:37 -0300

    Add project files.

---
+++ AspNet.Capitulo202.Razor/Models/ComentarioViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace AspNet.Capitulo202.Razor.Models
+{
+    public class ComentarioViewModel
+    {
+        public DateTime Data { get; set; }
+        public string Nome { get; set; }
+        public string Comentario { get; set; }
+    }
+}
