6aa4451bd3ab4423cf9ada569bfe2b34d914be5e
Barbateus <matteus.barbosa@yahoo.com.br>
2023-09-20 05:31:17 -0300

    Finalização do CRUD de Clientes

--- Marketplace.Mvc/Models/ClienteViewModel.cs
+++ Marketplace.Mvc/Models/ClienteViewModel.cs
@@ -10,13 +10,22 @@ namespace Marketplace.Mvc.Models
     {
         public int Id { get; set; }
 
-        [Required] 
+        [Required]
+        [StringLength(60, ErrorMessage = "O campo Nome pode ter no máximo 60 caracteres.", MinimumLength = 10)]
         public string Nome { get; set; }
+
         [Required]
+        [RegularExpression(@"^\d{3}.\d{3}.\d{3}-\d{2}$")]
         public string Documento { get; set; }
+
+
         [Required]
+        [StringLength(20, ErrorMessage ="O campo {0} pode ter no máximo {1} caracteres")]//{0}:nome do campo - {1}:máximo - {2}:mínimo
         public string Telefone { get; set; }
+
+        
         [Required]
+        [EmailAddress]
         public string Email { get; set; }
     }
 }
