bd21205231b4dcc1e65d2bb533035f175536f9c9
Barbateus <matteus.barbosa@yahoo.com.br>
2023-11-26 01:56:13 -0300

    Dominio + Repositório

---
+++ GatewayPagamento.Dominio/Entidades/Cartao.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GatewayPagamento.Dominio.Entidades
+{
+    public class Cartao
+    {
+        public int Id { get; set; }
+        public Guid Guid { get; set; } = Guid.NewGuid(); 
+        public string Numero { get; set;}
+        public Decimal Limite { get; set;}
+    }
+}
